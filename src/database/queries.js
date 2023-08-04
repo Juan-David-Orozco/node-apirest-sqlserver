2f095999ffa4c652895c66cae787790628b7629e
Juan-David-Orozco <ing.ele2018@gmail.com>
2023-08-04 06:23:30 -0500

    update queries - getTotalProducts

--- src/database/queries.js
+++ src/database/queries.js
@@ -1,8 +1,10 @@
+import config from '../config'
+
 export const queries = {
   getAllProducts : 'SELECT * FROM Products',
   getProductById: 'SELECT * FROM Products WHERE id = @id',
   addNewProduct : 'INSERT INTO Products (name, description, quantity) VALUES (@name, @description, @quantity)',
   removeProduct : 'DELETE FROM Products WHERE id = @id',
   editProduct : 'UPDATE Products SET name = @name, description = @description, quantity = @quantity WHERE id = @id',
-  getTotalProducts: 'SELECT COUNT(*) FROM [webstore].[dbo].[Products]'
+  getTotalProducts: `SELECT COUNT(*) FROM [${config.MSSQL.dbname}].[dbo].[Products]`
 }
